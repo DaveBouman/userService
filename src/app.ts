9052a307cd0091eb4afdee05f75f3ddfc142438d
Dave Bouman <49709450+DaveBouman@users.noreply.github.com>
2022-05-26 12:55:00 +0200

    added

--- src/app.ts
+++ src/app.ts
@@ -48,6 +48,12 @@ app.use(bodyParser.json({
         req.rawBody = buf
     }
 }));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(passport.initialize());
 app.use(passport.session());
 app.use('/api/v1/users', routes);
