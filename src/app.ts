752d4a50a38caa2a6b7f168e8eb129ae87ba3391
Dave Bouman <49709450+DaveBouman@users.noreply.github.com>
2022-05-27 11:26:52 +0200

    Changed cors back

--- src/app.ts
+++ src/app.ts
@@ -43,9 +43,9 @@ app.use(
 // app.use(morganMiddleware);
 // app.use(cors(corsOptions));
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'localhost:3000'); //replace localhost with actual host
-    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 app.use(helmet());
