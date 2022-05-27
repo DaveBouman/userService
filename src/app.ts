37694dab3fbd3bf47baee33b65bf70cac1b29afa
Dave Bouman <49709450+DaveBouman@users.noreply.github.com>
2022-05-27 11:36:23 +0200

    cors!@

--- src/app.ts
+++ src/app.ts
@@ -45,7 +45,7 @@ app.use(
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept X-Requested-With,content-type,**Authorization**");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type");
     next();
 });
 app.use(helmet());
