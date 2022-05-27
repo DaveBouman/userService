13ebfb61f6f3315ab6136b11a1f270c8f8ed7a80
Dave Bouman <49709450+DaveBouman@users.noreply.github.com>
2022-05-27 11:32:17 +0200

    aded headers

--- src/app.ts
+++ src/app.ts
@@ -44,8 +44,8 @@ app.use(
 // app.use(cors(corsOptions));
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept X-Requested-With,content-type,**Authorization**");
     next();
 });
 app.use(helmet());
