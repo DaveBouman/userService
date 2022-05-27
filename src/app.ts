bc17c3a68223809deddeb28a812adf2de1824a22
Dave Bouman <49709450+DaveBouman@users.noreply.github.com>
2022-05-27 11:59:14 +0200

    another cors

--- src/app.ts
+++ src/app.ts
@@ -44,7 +44,7 @@ app.use(
 // app.use(cors(corsOptions));
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, HEAD");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH, HEAD");
     res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     res.header('Access-Control-Allow-Credentials', true);
     next();
