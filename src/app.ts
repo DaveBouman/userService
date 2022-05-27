c5e750e66fb2dfa6a4f116f299b8c4022dc1a6fb
Dave Bouman <49709450+DaveBouman@users.noreply.github.com>
2022-05-27 12:21:41 +0200

    cors

--- src/app.ts
+++ src/app.ts
@@ -43,9 +43,10 @@ app.use(
 // app.use(morganMiddleware);
 // app.use(cors(corsOptions));
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, content-Type");
+    res.header("Access-Control-Allow-Credentials", true);
     next();
 });
 app.use(helmet());
